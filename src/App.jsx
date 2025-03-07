d9cf598a9873dd6edcbcba384630d83de3c24e38
RONIT <balironit@gmail.com>
2025-03-07 22:11:53 +0530

    feat:analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router'
 import Home from './pages/Home/Home'
 import AgentsCard from './pages/AgentCard/agentCard'
+import { Analytics } from '@vercel/analytics/react';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
     {/* <Route path='/' element={<AgentsCard/>} /> */}
     <Route path='/agents' element={<AgentsCard/>} />
   </Routes>
+  <Analytics/>
   </Router>
 
   )
